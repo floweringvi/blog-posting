40b926ade2c52b5fc5fc54868ed55f865038be96
Violet Stanziani <lovelylinguist@Violets-Air.fios-router.home>
2024-06-19 01:30:50 -0400

    fussing with light dark toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,8 @@ function redirectionFunc(event) {
     event.preventDefault();
     if(!Title.val()|| !userName.val() || !blogContent.val()) {
         alert("Must input all fields!")
-       $(location).attr('href','./index.html')
+       $(location).attr('href','./index.html');
+       return;
     }
     console.log("Hi how are you?")
     localStorage.setItem('title', Title.val())
@@ -19,3 +20,34 @@ function redirectionFunc(event) {
     $(location).attr('href', postURL)
 }
 postButton.on('click', redirectionFunc) 
+
+// wholePage = $('.entirety')
+Togglebtn= $('#lightordark')
+// document.getElementById('lightordark').addEventListener('click',()=>{
+//     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+//         document.documentElement.setAttribute('data-bs-theme','light')
+//     }
+//     else {
+//         document.documentElement.setAttribute('data-bs-theme','dark')
+//     }
+// })
+function lightorDark(){
+    if(html.attr('data-bs-theme')=== 'dark') {
+        html.attr('data-bs-theme','light')
+    } else {
+        html.attr('data-bs-theme','dark')
+    }
+
+}
+// mode= "dark"
+// function lightorDark(){
+//     if (mode ="dark") {
+//         mode="light";
+//       wholePage.attr('data-bs-theme','light');
+
+//     } else {
+//         mode="dark";
+//         wholePage.attr('data-bs-theme','dark');
+//     }
+// }
+Togglebtn.on('click', lightorDark())
--- assets/js/post.js
+++ assets/js/post.js
@@ -1,6 +1,30 @@
 postContainer = $('#postplace')
+let Blogposts=[]
+if(JSON.parse(localStorage.getItem(Blogposts) != [])) {
+   for( let i = 0; i < Blogposts.length; i++) {
+   Blogposts[i].userCreator = userName
+   Blogposts[i].blogTitle = postTitle
+   Blogposts[i].postContent =BlogPostText
+   postCard1 =$('<div>');
+   username2 =$('<h1>');
+   username2.text(userName);
+   postTitle2 =$('<h2>');
+   postTitle2.text(postTitle);
+   blogPostContent = $('<p>');
+   blogPostContent.text(BlogPostText);
+   postCard1.append(username2);
+   postCard1.append(postTitle2);
+   postCard1.append(blogPostContent);
+   postContainer.append(postCard1);
+  
 
+   }
+ } else {blogPostbuild()}
 function blogPostbuild () { 
+    // if(localStorage.getItem('blogpost') != []) {
+    //    postContainer.append
+    // }
+
       Username = localStorage.getItem('username')
      postTitle = localStorage.getItem('title')
      blogPost = localStorage.getItem('blogpost') 
@@ -22,10 +46,19 @@ function blogPostbuild () {
     // userCreator.append(blogTitle);
     // blogTitle.append(postContent);
     postContainer.append(postCard);
+    let BlogpostContent = {
+        userCreator: Username,
+        blogTitle: postTitle,
+        postContent: blogPost
+    }
+  
+    Blogposts.push(BlogpostContent)
+    console.log(Blogposts)
 
+    localStorage.setItem('Blogposts', JSON.stringify(Blogposts));
 
 }
-blogPostbuild();
+blogPostbuild()
 const homepage = './index.html'
 homeButton = $('#homeplanet')
 function returnHome() {
